3baafad42993fd03e9070fbb267d83667e3c75e1
James Beston <james@beston.me>
2024-08-15 18:09:29 +0100

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import localFont from 'next/font/local';
 import './globals.css';
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={geist.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
