b858f942cdffb0dcd432c2fda10286a7f0034231
James Beston <james@beston.me>
2024-08-15 11:23:31 +0100

    Update URL on Icon utility

--- app/lib/utils/icon.tsx
+++ app/lib/utils/icon.tsx
@@ -7,9 +7,11 @@ export default function Icon({
   size: number;
   weight: string;
 }) {
+
+  const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.VERCEL_PROJECT_PRODUCTION_URL;
   return (
     <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} aria-hidden>
-      <use href={`/icons/${weight}.svg#${icon} `} fill='currentColor' />
+      <use href={`${url}/icons/${weight}.svg#${icon} `} fill='currentColor' />
     </svg>
   );
 }
