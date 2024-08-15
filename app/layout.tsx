aa109832b041728365c705dcfdae0161c20f9f8b
James Beston <james@beston.me>
2024-08-15 17:59:05 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import localFont from 'next/font/local';
 import './globals.css';
 
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={geist.className}>{children}</body>
+      <body className={geist.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
